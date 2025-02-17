3ab0b6fa8f91371bb27e211e68e347d1fdba8a0e
mmingyeomm <aliveck0208@gmail.com>
2025-02-18 01:45:17 +0900

    interaction

--- scripts/deployPoolStorage.ts
+++ scripts/deployPoolStorage.ts
@@ -5,16 +5,31 @@ import { compile, NetworkProvider } from '@ton/blueprint';
 export async function run(provider: NetworkProvider) {
     // Compile the PoolStorage contract
     const poolStorage = provider.open(
-            PoolStorage.createFromCode(
-                await compile('PoolStorage')
-            )
-        );
-    
-    // Deploy the contract with 0.05 TON
-    await poolStorage.sendDeploy(provider.sender(), toNano('0.05'));
+        PoolStorage.createFromCode(
+            await compile('PoolStorage')
+        )
+    );
+
+    // Define the data to store
+    const value = toNano('0.002'); // Value in NanoTON (adjust as needed)
+    const queryID = 5; // Query ID
+
+//  이거는 userAddress와 userAmount 가져오는 함수 호출 이것도 됨
+    const getResult = await poolStorage.getSupply();
+
+    console.log(getResult);
+
+
+
+    // 이거는 됨. Store Data함수 호출 됨
+    // const result = await poolStorage.sendStoreData(provider.sender(), { value, queryID });
+
+    // console.log(result);
+
+
 
     // Wait for the contract to be deployed
-    await provider.waitForDeploy(poolStorage.address);
+    // await provider.waitForDeploy(poolStorage.address);
 
-    console.log('PoolStorage deployed at address:', poolStorage.address.toString());
+    // console.log('PoolStorage deployed at address:', poolStorage.address.toString());
 }
