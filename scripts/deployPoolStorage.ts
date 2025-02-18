f05551e46c13e0a22e12d145dce44e692352bc7c
mmingyeomm <aliveck0208@gmail.com>
2025-02-18 13:39:13 +0900

    storage

--- scripts/deployPoolStorage.ts
+++ scripts/deployPoolStorage.ts
@@ -10,15 +10,18 @@ export async function run(provider: NetworkProvider) {
         )
     );
 
-    // Define the data to store
-    const value = toNano('0.002'); // Value in NanoTON (adjust as needed)
-    const queryID = 5; // Query ID
+
+    await poolStorage.sendDeploy(provider.sender(), toNano('0.05'));
 
 //  이거는 userAddress와 userAmount 가져오는 함수 호출 이것도 됨
     const getResult = await poolStorage.getSupply();
 
     console.log(getResult);
 
+
+    // Define the data to store
+    // const value = toNano('0.002'); // Value in NanoTON (adjust as needed)
+    // const queryID = 5; // Query ID
 
 
     // 이거는 됨. Store Data함수 호출 됨
